6f0935c95af8233e5f187ed2839ef45cd70998d4
Mey0312 <meyvelazda@gmail.com>
2024-04-03 09:41:28 -0600

    Fin sección 2

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,14 @@
+
+
+
+export interface infoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  pinterest?:string
+  twitter?: string;
+  instagram?: string;
+  equipo_trabajo?: any[];
+}
---
+++ src/app/services/infoPagina.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { infoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoPaginaService {
+  info: infoPagina = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+    console.log('Servicio de InfoPagina listo');
+
+    //modulo para leer un archivo json y tomar sus
+    //propiedades para ser utilisados en la paginasS/peticiones http
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe( (resp: infoPagina) => {
+
+      // console.log( resp[facebook] );
+
+      // .subscribe((resp: any) => {
+
+        this.cargada = true;
+        this.info = resp;
+       console.log(resp);
+        //  console.log( resp.facebook );
+
+        // });
+     });
+  }
+}
